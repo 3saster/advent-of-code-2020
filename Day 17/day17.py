fdb88b6b08d7ba8c55491837d4e91d3088cd2207
PaulyB <43163391+3saster@users.noreply.github.com>
2020-12-17 19:38:11 -0800

    Day 17

--- Day 17/day17.py
+++ Day 17/day17.py
@@ -5,9 +5,9 @@ import operator
 def addTuple(t1,t2):
     return type(t1)(map(operator.add, t1, t2))
 
-def cubeSol(lines,n,dim4):
+def cubeSol(lines,n,dim):
     # Make Cube Dict
-    keys = [(y,x,0,0) for y,l in enumerate(lines) for x,_ in enumerate(l) if lines[x][y]=='#']
+    keys = [(y,x)+(0,)*(dim-2) for y,l in enumerate(lines) for x,_ in enumerate(l) if lines[x][y]=='#']
 
     # Init Active Cubes
     cubes = defaultdict(lambda: [False,0,False,0])
@@ -15,38 +15,38 @@ def cubeSol(lines,n,dim4):
         cubes[k] = [True,0,True,0]
     # Init Neighbors
     loopKeys = list(cubes.keys())
+    neighborRanges = set(product( (-1,0,1), repeat=dim ))
+    neighborRanges.remove( (0,)*dim )
+
     for c in loopKeys:
         if cubes[c][0]:
-            for x,y,z,w in product( (-1,0,1),(-1,0,1),(-1,0,1),(-1,0,1) ):
-                if w!=0 and not dim4: continue 
-                if x==y==z==w==0: continue
-                cubes[ addTuple(c,(x,y,z,w)) ][1] += 1
-                cubes[ addTuple(c,(x,y,z,w)) ][3] += 1
+            for tup in neighborRanges:
+                cubes[ addTuple(c,tup) ][1] += 1
+                cubes[ addTuple(c,tup) ][3] += 1
     # Advance stages
     for _ in range(n):
-        advanceStage(cubes,dim4)
+        advanceStage(cubes,dim)
 
     return len([c for c in cubes.keys() if cubes[c][0]])
 
-def advanceStage(cubes,dim4):
+def advanceStage(cubes,dim):
     loopKeys = list(cubes.keys())
+    neighborRanges = set(product( (-1,0,1), repeat=dim ))
+    neighborRanges.remove( (0,)*dim )
+
     for c in loopKeys:
         # Active cube being deactivated
         if cubes[c][0] == True and ( cubes[c][1] != 2 and cubes[c][1] != 3 ):
             cubes[c][2] = False
-            for x,y,z,w in product( (-1,0,1),(-1,0,1),(-1,0,1),(-1,0,1) ):
-                if w!=0 and not dim4: continue
-                if x==y==z==w==0: continue
+            for tup in neighborRanges:
                 # neighbors of adjacent cubes go down 1 in next stage
-                cubes[ addTuple(c,(x,y,z,w)) ][3] -= 1
+                cubes[ addTuple(c,tup) ][3] -= 1
         # Inactive cube being activated
         elif cubes[c][0] == False and ( cubes[c][1] == 3 ):
             cubes[c][2] = True
-            for x,y,z,w in product( (-1,0,1),(-1,0,1),(-1,0,1),(-1,0,1) ):
-                if w!=0 and not dim4: continue 
-                if x==y==z==w==0: continue
+            for tup in neighborRanges:
                 # neighbors of adjacent cubes go up 1 in next stage
-                cubes[ addTuple(c,(x,y,z,w)) ][3] += 1
+                cubes[ addTuple(c,tup) ][3] += 1
 
     # Update neighbors in next stage
     loopKeys = list(cubes.keys())
@@ -64,14 +64,14 @@ def readInput():
     return lines
 
 def Part1(lines):
-    sol = cubeSol(lines,6,False)
+    sol = cubeSol(lines,6,3)
 
     print("Part 1:")
     print("\t" + str(sol))
     print()
 
 def Part2(lines):
-    sol = cubeSol(lines,6,True)
+    sol = cubeSol(lines,6,4)
 
     print("Part 2:")
     print("\t" + str(sol))
